96f55afabad601adaadb32894a212f8830eeac8a
Han <BNSSsgwh@dcsfen01.ccni.rpi.edu>
2022-02-23 11:52:48 -0500

    UPdated script

--- pycontrast/scripts/run_supcon_topk_mask.sh
+++ pycontrast/scripts/run_supcon_topk_mask.sh
@@ -16,14 +16,16 @@ python \
       --cosine \
       --data_folder /gpfs/u/locker/200/CADS/datasets/ImageNet/ \
       --multiprocessing-distributed \
-      --world-size 1 \
+      --world-size 3 \
       --rank 0 \
       --num_workers 32 \
       --ngpus 6 \
       --model_path $HOME2/scratch/PyContrast-TopkMask/pycontrast/saved_models/ \
       --tb_path $HOME2/scratch/PyContrast-TopkMask/pycontrast/logs/ \
       --topk_path $HOME2/scratch/PyContrast-TopkMask/pycontrast/imagenet_resnet50_top10.pkl \
-      --sup_mode topk-mask
+      --sup_mode topk-mask \
+      --save_freq 5 \
+      --resume $HOME2/scratch/PyContrast-TopkMask/pycontrast/saved_models/MoCov2_resnet50_RGB_Jig_False_moco_aug_B_mlp_0.2_cosine/current.pth
 
 echo "Run completed at:- "
 date
