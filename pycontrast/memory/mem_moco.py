Fixing negative masking bug

pycontrast/memory/mem_moco.py
@@ -76,7 +76,8 @@ class BaseMoCo(nn.Module):
         positives_mask = torch.eq(batch_labels, k_queue_labels.T).float().cuda()
         num_positives_per_row = torch.sum(positives_mask, dim=1)
 
-        topk_mask = torch.eq(topk_labels.unsqueeze(-1).permute(1,0,2), k_queue_labels.permute(1,0))
+        topk_mask = torch.stack([torch.eq(topk.unsqueeze(-1), k_queue_labels.permute(1,0)) for topk in torch.unbind(topk_labels, dim=1)], dim=0)
+        assert topk_mask == torch.eq(topk_labels.unsqueeze(-1).permute(1,0,2), k_queue_labels.permute(1,0))
         topk_mask = topk_mask.any(dim=0).float().cuda()
         negatives_mask = 1. - positives_mask
         negatives_mask = torch.stack([topk_mask, negatives_mask], dim=0).bool().all(dim=0).float()